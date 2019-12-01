f4a93c63da9a13e680d057769c74234e0327ee58
Sergey Dmitriev <sergey_dmitriev@epam.com>
2019-12-01 14:54:33 +0300

    Add promtAdd function

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,24 @@ function displayTodoList(): void {
 }
 
 enum Commands {
+  Add = "Add New Task",
   Toggle = "Show/Hide Completed",
   Quit = "Quit"
 }
 
+function promptAdd(): void {
+  console.clear();
+  inquirer.prompt({
+    type: "input",
+    name: "add",
+    message: "Enter task"
+  }).then(answers => {
+    if(answers["add"] !== ""){
+      collection.addTodo(answers["add"]);
+    }
+    promptUser();
+  })
+}
 function promptUser(): void {
     console.clear();
     displayTodoList();
@@ -35,6 +49,9 @@ function promptUser(): void {
         case Commands.Toggle:
           showCompleted = !showCompleted;
           promptUser();
+          break;
+        case Commands.Add:
+          promptAdd();
           break;
       }
     })
